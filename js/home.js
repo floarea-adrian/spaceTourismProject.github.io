2a7a623482bc889758f067001fa75f8526fa1164
floarea-adrian <floareaadrian02@yahoo.com>
2024-08-02 17:31:40 +0300

    modifies

--- js/home.js
+++ js/home.js
@@ -5,20 +5,20 @@ function setBackground() {
 
     if(window.innerWidth > 1200) {
         background.style.backgroundImage = "url('../assets/home/background-home-desktop.jpg')"
-        background.style.backgroundSize = "cover"
+        
 
       
     } else if (window.innerWidth > 500 ) {
         background.style.backgroundImage = "url('../assets/home/background-home-tablet.jpg')"
-        background.style.backgroundSize = "100% 700px";
 
         
     } else {
         background.style.backgroundImage = "url('../assets/home/background-home-mobile.jpg')"
-        background.style.backgroundSize = "cover";
+        
 
         
     }
+    background.style.backgroundSize = "cover";
     background.style.backgroundRepeat = "no-repeat";
 
 }
