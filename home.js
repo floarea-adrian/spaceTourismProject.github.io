41f1f827c4bddc8a99a1031fa714e9f5037b146d
floarea-adrian <floareaadrian02@yahoo.com>
2024-08-03 21:47:11 +0300

    mod 6

--- home.js
+++ home.js
@@ -4,15 +4,15 @@ let background = document.getElementById("homeBackground-responsive")
 function setBackground() {
 
     if(window.innerWidth > 1200) {
-        background.style.backgroundImage = "url('../assets/home/background-home-desktop.jpg')"
+        background.style.backgroundImage = "url('./assets/home/background-home-desktop.jpg')"
         background.style.backgroundSize = "cover";
    
     } else if (window.innerWidth > 500 ) {
-        background.style.backgroundImage = "url('../assets/home/background-home-tablet.jpg')"
+        background.style.backgroundImage = "url('./assets/home/background-home-tablet.jpg')"
         background.style.backgroundSize = "100% 700px";
   
     } else {
-        background.style.backgroundImage = "url('../assets/home/background-home-mobile.jpg')"
+        background.style.backgroundImage = "url('./assets/home/background-home-mobile.jpg')"
         background.style.backgroundSize = "cover";
      
     }
